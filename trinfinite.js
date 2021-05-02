f2d7ac53b0d4b7d56696eb472c64eed74ca17071
harrybp <harrytbp@gmail.com>
2021-05-03 00:10:32 +0100

    Interface Finished

--- trinfinite.js
+++ trinfinite.js
@@ -9,7 +9,7 @@ function seededRandom(a) {
 
 // ----------------------------------------------------------------------------
 // Colour function for the triangles
-const tiledColourFunc = (jitterFactor = 0.15) => ({ xPercent, yPercent, xScale, yScale, opts, random }) => {
+const tiledColourFunc = (jitterFactor, gradientFactor) => ({ xPercent, yPercent, xScale, yScale, opts, random }) => {
     let xDist = 0;
     let yDist = 0;
 
@@ -26,39 +26,43 @@ const tiledColourFunc = (jitterFactor = 0.15) => ({ xPercent, yPercent, xScale, 
     }
 
     // Use the distance to seed the jitter
-    const a = xScale((xDist*4) + (jitterFactor * seededRandom(parseInt(xDist * 100))));
-    const b = yScale((yDist*4) + (jitterFactor * seededRandom(parseInt(yDist * 100)))); //
+    const a = xScale((xDist*gradientFactor) + (jitterFactor * seededRandom(parseInt(xDist * 100))));
+    const b = yScale((yDist*gradientFactor) + (jitterFactor * seededRandom(parseInt(yDist * 100)))); //
 
     return chroma.mix(a, b, 0.5, opts.colorSpace);
 };
 
 // ----------------------------------------------------------------------------
-function getTriangles(width, height, cellsize){
+// Generate a basic pattern to get the points data
+function getTriangles(width, height, cellSize, variance){
     // Generate a pattern of correct size and use its triangles
     const pattern = trianglify({
-      cellSize: 20,
+      cellSize: cellSize,
       width: oldWidth,
-      height: oldHeight
+      height: oldHeight,
+      variance: variance,
     })
     return pattern.points;
 }
 
 // ----------------------------------------------------------------------------
+// Find the maximum x and y values of the points
 function getDimensions(points){
 
     // Find the min and max of the points
     var maxX = -99;
     var maxY = -99;
-    for(var i = 0; i < oldPoints.length; i++){
-        if(oldPoints[i][0] > maxX)
-            maxX = oldPoints[i][0];
-        if(oldPoints[i][1] > maxY)
-            maxY = oldPoints[i][1];
+    for(var i = 0; i < points.length; i++){
+        if(points[i][0] > maxX)
+            maxX = points[i][0];
+        if(points[i][1] > maxY)
+            maxY = points[i][1];
     }
     return [maxX, maxY];
 }
 
 // ----------------------------------------------------------------------------
+// Tile the points together into 4 quadrants
 function tilePoints(points, width, height){
     const newPoints = []
     for(var i = 0; i < points.length; i++){
@@ -82,6 +86,7 @@ function tilePoints(points, width, height){
 }
 
 // ----------------------------------------------------------------------------
+// Extract the center square and tile
 function getCanvas(pattern, xMult, yMult){
     var canvas = pattern.toCanvas();
     const ctx = canvas.getContext('2d');
@@ -100,72 +105,163 @@ function getCanvas(pattern, xMult, yMult){
     return canvas1;
 }
 
+var newHeight = 0;
+var newWidth = 0;
 
-var mainWindow = document.getElementById("main");
+function getPoints(width, height, cellSize, variance){
+    // Generate some triangles
+    var oldPoints = getTriangles(width, height, cellSize, variance);
+
+    // Get dimensions
+    var dimensions = getDimensions(oldPoints);
+    newHeight = dimensions[1];
+    newWidth = dimensions[0];
+
+    // Tile the points into 4 quadrants
+    var points = tilePoints(oldPoints, newWidth, newHeight);
+    return points;
+}
+
+// ----------------------------------------------------------------------------
+
+var mainWindow = document.getElementById("mainWindow");
+console.log(mainWindow);
 var oldHeight = window.innerHeight * 0.3;
 var oldWidth = oldHeight;
 //var oldWidth = window.innerWidth * 0.3
-
-// Generate some triangles
-var oldPoints = getTriangles(oldWidth, oldHeight, 20);
-
-// Get dimensions
-var dimensions = getDimensions(oldPoints);
-var newHeight = dimensions[1];
-var newWidth = dimensions[0];
-
-// Tile the points into 4 quadrants
-var points = tilePoints(oldPoints, newWidth, newHeight);
-
-const palette = {mine:  ["#9AC71B", "#B3A22A", "#189830", "#79581E"]};
-
-var newPattern = trianglify({
-    height: newHeight*2,
-    width: newWidth*2,
-    points: points,
-    variance: 0.7,
-    palette: palette,
-    colorFunction: tiledColourFunc(0.3)
-});
-const canvas = getCanvas(newPattern, 1, 1);
-document.body.appendChild(canvas);
-
-var dataURL = canvas.toDataURL("image/png");
-document.body.style.backgroundImage = "url('"+dataURL+"')";
+//
+var cellSizeValue = 20;
+var varianceValue = 0.75;
+var points = getPoints(oldWidth, oldHeight, cellSizeValue, varianceValue);
 
 
+// Variable Inputs
+var palette = {mine:  ["#9AC71B", "#B3A22A", "#189830", "#79581E"]};
+var jitterValue = 0.5;
+var gradientValue = 4;
 
-/*
-document.body.appendChild(newPattern.toCanvas())
+// ----------------------------------------------------------------------------
+// Refresh the pattern
+function getPattern(){
+    var newPattern = trianglify({
+        height: newHeight*2,
+        width: newWidth*2,
+        points: points,
+        palette: palette,
+        colorFunction: tiledColourFunc(jitterValue, gradientValue)
+    });
+    return newPattern;
+}
 
-const canvas = document.getElementsByTagName("canvas")[0]
-const ctx = canvas.getContext('2d');
-var canvasW = canvas.width
-var canvasH = canvas.height
-var imageData = ctx.getImageData(canvasW * 0.25,canvasH * 0.25,canvasW * 0.75, canvasH * 0.75);
-let canvas1 = document.createElement("canvas");
-canvas1.width = canvasW;
-canvas1.height = canvasH;
-let ctx1 = canvas1.getContext("2d");
-ctx1.putImageData(imageData, 0, 0);
-ctx1.putImageData(imageData, canvasW * 0.5, 0);
-ctx1.putImageData(imageData, 0, canvasH * 0.5);
-ctx1.putImageData(imageData, canvasW * 0.5, canvasH * 0.5);
+// ----------------------------------------------------------------------------
+// Updat the pattern and apply it to the background
+function updatePattern(){
+    var newPattern = getPattern();
+    const canvas = getCanvas(newPattern, 1, 1);
+    var dataURL = canvas.toDataURL("image/png");
+    mainWindow.style.backgroundImage = "url('"+dataURL+"')";
 
-document.body.appendChild(canvas1);
+}
+updatePattern();
 
-let canvas2 = document.createElement("canvas");
-canvas2.width = canvasW/2;
-canvas2.height = canvasH/2;
-let ctx2 = canvas2.getContext("2d");
-ctx2.putImageData(imageData, 0, 0);
-document.body.appendChild(canvas2);
+// ----------------------------------------------------------------------------
+// Update the jitter factor from the slider
+function updateJitter(){
+    var jitterRange = document.getElementById("jitterRange");
+    var jitterRangeLabel = document.getElementById("jitterRangeLabel");
+    jitterRangeLabel.innerHTML = "Jitter Factor: " + jitterRange.value;
+    jitterValue = jitterRange.value;
+    updatePattern();
+}
 
-var dataURL = canvas2.toDataURL("image/png");
-//var newTab = window.open('about:blank','image from canvas');
-//newTab.document.write("<img src='" + dataURL + "' alt='from canvas'/>");
-//window.open(canvas2.toDataURL("image/png"));
+// ----------------------------------------------------------------------------
+// Update the gradient factor from the slider
+function updateGradientFactor(){
+    var gradientFactorRange = document.getElementById("gradientFactorRange");
+    var gradientFactorRangeLabel = document.getElementById("gradientFactorRangeLabel");
+    gradientFactorRangeLabel.innerHTML = "Gradient Factor: " + gradientFactorRange.value;
+    gradientValue = gradientFactorRange.value;
+    updatePattern();
+}
 
-console.log("Start: (oldWidth: " + oldWidth + "), (oldHeight: " + oldHeight + ")")
+// ----------------------------------------------------------------------------
+// Update the cell size from the slider
+function updateCellSize(){
+    var cellSizeRange = document.getElementById("cellSizeRange");
+    var cellSizeRangeLabel = document.getElementById("cellSizeRangeLabel");
+    cellSizeRangeLabel.innerHTML = "Cell Size: " + cellSizeRange.value;
+    cellSizeValue = cellSizeRange.value;
+    points = getPoints(oldWidth, oldHeight, cellSizeValue, varianceValue);
+    updatePattern();
+}
 
-console.log("Derived: (newWidth: " + newWidth.toFixed(1) + "), (newHeight: " + newHeight.toFixed(1) + ")")*/
+// ----------------------------------------------------------------------------
+// Update the variance from the slider
+function updateVariance(){
+    var varianceRange = document.getElementById("varianceRange");
+    var varianceRangeLabel = document.getElementById("varianceRangeLabel");
+    varianceRangeLabel.innerHTML = "Variance: " + varianceRange.value;
+    varianceValue = varianceRange.value;
+    points = getPoints(oldWidth, oldHeight, cellSizeValue, varianceValue);
+    updatePattern();
+}
+
+// ----------------------------------------------------------------------------
+// Update the colour palette from the colour pickers
+function updateColours(){
+    var inputs = document.getElementsByTagName("input");
+    var newColours = [];
+    for(var i = 0; i < inputs.length; i++) {
+        if(inputs[i].type.toLowerCase() == "color") {
+            newColours.push(inputs[i].value);
+        }
+    }
+    palette = {mine: newColours};
+    updatePattern();
+}
+
+// ----------------------------------------------------------------------------
+// Download image
+function downloadImage(data, filename = "untitled.png") {
+    var a = document.createElement('a');
+    a.href = data;
+    a.download = filename;
+    document.body.appendChild(a);
+    a.click();
+}
+
+// ----------------------------------------------------------------------------
+// Save the generated image
+function saveImage(){
+    var pattern = getPattern();
+    var canvas = getCanvas(pattern, 1, 1);
+    var dataURL = canvas.toDataURL("image/png", 1.0);
+    downloadImage(dataURL, "triangles.png");
+}
+
+var numColours = 4;
+
+// ----------------------------------------------------------------------------
+// Add a colour to the palette
+function addColour(){
+    var picker = document.createElement("input");
+    picker.type = "color";
+    picker.name = "colour" + numColours;
+    picker.id = "colour" + numColours;
+    picker.value = "#123456";
+    picker.setAttribute("onchange", "updateColours()");
+    var allPickers = document.getElementById("colourPickers");
+    allPickers.appendChild(picker);
+    numColours++;
+    updateColours();
+}
+
+// ----------------------------------------------------------------------------
+// Remove a colour from the palette
+function removeColour(){
+    var allPickers = document.getElementById("colourPickers");
+    var toRemove = document.getElementById("colour" + (numColours - 1));
+    allPickers.removeChild(toRemove);
+    numColours--;
+    updateColours();
+}
